6281f13e70b96c2a6a87c0241afc3fb51927ce37
AlirezaRa <alireza.rafiee94@gmail.com>
2015-02-20 20:15:44 -0800

    Update aes.py

--- src/aes.py
+++ src/aes.py
@@ -36,7 +36,7 @@ def baseNto10(num, base):
     num.reverse()
     for i in range(len(num)):
         try:
-            if int(num[i]) in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]:
+            if int(num[i]) in range(10):
                 result += int(num[i]) * base ** int(i)
         except:
             for j in range(10, 48):
