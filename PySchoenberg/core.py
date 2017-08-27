bd8056dc041b1d65f0c231fa10871a19d7e697b7
Alireza Rafiei <mail@rafiei.net>
2017-08-27 07:35:11 -0700

    additional parsers and methods for rows and also implementing base matrix

--- PySchoenberg/core.py
+++ PySchoenberg/core.py
@@ -3,7 +3,6 @@ _ORDERED_NOTE_REPRS = tuple(["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#",
 
 
 class Note(object):
-
     def parse(pretty_note):
         return Note(pretty_note[0], bool(pretty_note[-1] == "#"))
 
@@ -28,6 +27,9 @@ class Note(object):
             return other.is_sharp
         return self.name < other.name
 
+    def __int__(self):
+        return _ORDERED_NOTE_REPRS.index(self.__repr__()) + 1
+
 
 class Row(object):
     def __init__(self, row_list=_ORDERED_NOTE_REPRS):
@@ -37,8 +39,25 @@ class Row(object):
     def shift_row(self, amount):
         self.__row = (self.__row[amount:] + self.__row[:amount])
 
+    def to_numerical(self):
+        return tuple(map(int, self))
+
+    def parse_numerical(numerical_row):
+        return Row([
+            _ORDERED_NOTE_REPRS[number - 1]
+            for number in numerical_row
+        ])
+
+    def corresponding_column(self, arity):
+        numerical_column = [int(self.__row[arity - 1])]
+        for i in range(1, 12):
+            candidate = ((numerical_column[-1] - (int(self.__row[i]) -
+                                                  int(self.__row[i - 1]))) % 12)
+            numerical_column.append(12 if candidate == 0 else candidate)
+        return Row.parse_numerical(numerical_column)
+
     def __eq__(self, other):
-        return [self[index] == other[index] for index in range(12)]
+        return all([self[index] == other[index] for index in range(12)])
 
     def __iter__(self):
         return self.__row.__iter__()
@@ -48,3 +67,20 @@ class Row(object):
 
     def __repr__(self):
         return "Row{}".format(self.__row)
+
+
+class Matrix(object):
+    def __init__(self, base_row):
+        # Looping through correspondign columns generates a transposed matrix
+        # if it's fed with the base_row directly since there's no specific
+        # representation of a column in Python and they must be represented as
+        # rows. Also, the corresponding column of first arity of the
+        # first column, would by definition be equivalent to the generating
+        # base row itself. So to have the proper matrix, we'd feed it the
+        # corresponding column of the first arity of the base_row instead of
+        # base_row itself.
+        transformed_base_row = base_row.corresponding_column(1)
+        self.__matrix = [
+            transformed_base_row.corresponding_column(arity)
+            for arity in range(1, 13)
+        ]
